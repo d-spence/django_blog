956bd830391027dc42f287321be2186f3bc810c7
Dan Spencer <dspence180@gmail.com>
2020-08-10 15:10:32 -0700

    Tutorial 3 End

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Dummy data
+posts = [
+    {
+        'author': 'Dan Spencer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 10, 2020'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 11, 2020'
+    }
+]
+
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts,
+        'title': 'Home'
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    context = {
+        'title': 'About'
+    }
+    return render(request, 'blog/about.html', context)
